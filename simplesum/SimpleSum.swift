9369b0681c64395a29a1ffaa26b246e43ecc2718
Sahil Sinha <sahilsinha.dar@gmail.com>
2024-07-07 16:46:58 +0530

    init: initialised project with simpleSum method
    - initialised repository & added a simpleSum method

---
+++ simplesum/SimpleSum.swift
@@ -0,0 +1,14 @@
+//
+//  SimpleSum.swift
+//  simplesum
+//
+//  Created by Sahil Sinha on 07/07/24.
+//
+
+import Foundation
+
+
+// Simple method which returns sum of two numbers
+public func simpleSum(a: Int, b: Int) -> Int {
+  return a + b
+}
---
+++ simplesum/simplesum.h
@@ -0,0 +1,18 @@
+//
+//  simplesum.h
+//  simplesum
+//
+//  Created by Sahil Sinha on 07/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for simplesum.
+FOUNDATION_EXPORT double simplesumVersionNumber;
+
+//! Project version string for simplesum.
+FOUNDATION_EXPORT const unsigned char simplesumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <simplesum/PublicHeader.h>
+
+
